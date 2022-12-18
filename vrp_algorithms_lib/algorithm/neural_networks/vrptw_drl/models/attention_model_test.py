bdbfb28daccdd200136166dc0037ddebdc32ea5a
Elisey Sudakov <elisey_sudakov@mail.ru>
2022-12-18 13:34:25 +0300

    better

--- vrp_algorithms_lib/algorithm/neural_networks/vrptw_drl/models/attention_model_test.py
+++ vrp_algorithms_lib/algorithm/neural_networks/vrptw_drl/models/attention_model_test.py
@@ -1,9 +1,11 @@
 from vrp_algorithms_lib.algorithm.neural_networks.vrptw_drl.test_utils import problem_state_for_tests
 from vrp_algorithms_lib.problem.test_utils import problem_description_for_tests
-from vrp_algorithms_lib.algorithm.neural_networks.vrptw_drl.objects import ProblemState
+from vrp_algorithms_lib.algorithm.neural_networks.vrptw_drl.objects import ProblemState, Action
 from vrp_algorithms_lib.algorithm.neural_networks.vrptw_drl.models.attention_model import AttentionModel
+from vrp_algorithms_lib.problem.models import LocationId, CourierId
 
 import pytest
+import numpy as np
 
 
 @pytest.fixture
@@ -36,6 +38,27 @@ def test_get_vehicles_state_information(problem_state_for_tests, attention_model
         problem_state=problem_state_for_tests
     )
 
+    depot = list(problem_state_for_tests.problem_description.depots.values())[0]
+    depot_lat = depot.lat
+    depot_lon = depot.lon
     assert len(vehicles_state_information) == len(problem_state_for_tests.problem_description.couriers)
+    assert np.isclose(vehicles_state_information[0][0].item(),
+                      attention_model_without_neural_network.normalize_lat(depot_lat))
+    assert np.isclose(vehicles_state_information[0][1].item(),
+                      attention_model_without_neural_network.normalize_lon(depot_lon))
     assert vehicles_state_information[0][2].item() == 0,  'normalized number of delivered orders is 0 initially'
     assert vehicles_state_information[0][3].item() == 0,  'normalized travel distance is 0 initially'
+
+    action = Action(location_id=LocationId('location_1'), courier_id=CourierId('courier_1'))
+    problem_state_for_tests.update(action)
+
+    vehicles_state_information = attention_model_without_neural_network.get_vehicles_state_information(
+        problem_state=problem_state_for_tests
+    )
+    location_1 = problem_state_for_tests.problem_description.locations[LocationId('location_1')]
+    location_1_lat = location_1.lat
+    location_1_lon = location_1.lon
+    assert np.isclose(vehicles_state_information[0][0].item(),
+                      attention_model_without_neural_network.normalize_lat(location_1_lat))
+    assert np.isclose(vehicles_state_information[0][1].item(),
+                      attention_model_without_neural_network.normalize_lon(location_1_lon))
