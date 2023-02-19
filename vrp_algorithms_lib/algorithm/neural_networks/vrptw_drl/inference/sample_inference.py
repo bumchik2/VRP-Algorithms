f510a9d1d72293c9f0ba98b150ccf681e0c3a9ac
Elisey Sudakov <elisey_sudakov@mail.ru>
2023-02-19 12:24:36 +0300

    epsilon greedy strat in sample inference

--- vrp_algorithms_lib/algorithm/neural_networks/vrptw_drl/inference/sample_inference.py
+++ vrp_algorithms_lib/algorithm/neural_networks/vrptw_drl/inference/sample_inference.py
@@ -1,16 +1,53 @@
+from typing import Optional
+
 import numpy as np
 import torch
 
 from vrp_algorithms_lib.algorithm.neural_networks.vrptw_drl.inference.sequential_inference import SequentialInference
+from vrp_algorithms_lib.algorithm.neural_networks.vrptw_drl.models.model_base import ModelBase
+from vrp_algorithms_lib.problem.models import ProblemDescription
+from vrp_algorithms_lib.problem.models import Routes
 
 
 class SampleInference(SequentialInference):
+    def __init__(
+            self,
+            model: ModelBase,
+            problem_description: ProblemDescription,
+            routes: Optional[Routes],
+            epsilon: float = 1
+    ):
+        super().__init__(
+            model,
+            problem_description,
+            routes
+        )
+
+        self._epsilon = epsilon
+
+    def set_epsilon(
+            self,
+            epsilon: float
+    ):
+        self._epsilon = epsilon
+
+    def multiply_epsilon(
+            self,
+            multiplier: float
+    ):
+        self._epsilon *= multiplier
+
     def choose_courier_idx(
             self,
             courier_logits: torch.Tensor
     ) -> int:
         p = torch.nn.Softmax()(courier_logits).to(torch.device('cpu')).numpy()
-        return np.random.choice(range(len(courier_logits)), p=p)
+
+        gen = np.random.uniform()
+        if gen <= self._epsilon:
+            return np.random.choice(range(len(courier_logits)), p=p)
+        else:
+            return np.argmax(p)
 
     def choose_location_idx(
             self,
